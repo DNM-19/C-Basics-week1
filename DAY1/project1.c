e3a6c732ef24920964133046ea2d86a2254c235c
DNM-19 <mulyashidennis@gmail.com>
2026-01-19 19:25:44 +0200

    Update project1.c

--- DAY1/project1.c
+++ DAY1/project1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main()
-{
+
+int main(){
     char name[30];
     int age;
     char hobby[30];
@@ -11,5 +11,5 @@ int main()
     printf("What's your favourite hobby: ");
     scanf("%s", hobby);
     printf("Hello %s, You are %d years old your favourite hobby is %s.\n", name, age, hobby);
-    return 0;
+return 0;
 }
